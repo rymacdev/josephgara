93d6ec80e7591c1ddf3767d1b4be7847bdcd44aa
Ryan Macalandag <rmacalandag@gmail.com>
2023-10-03 23:21:52 +1100

    fix height

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { SocialIcon } from "react-social-icons"
 function App() {
 
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="flex flex-col w-full">
       <header className="pt-12 pb-8 md:py-14 text-center text-slate-900">
         <h3 className="py md:py-2 text-2xl md:text-3xl font-bold uppercase tracking-widest">This is</h3>
         <h1 className="text-4xl md:text-6xl font-bold">Joseph Gara</h1>
       </header>
-      <div className="flex flex-row justify-center items-center h-2/3 overflow-hidden">
+      <div className="flex flex-row justify-center items-center h-3/5 overflow-hidden">
         <img className="object-cover max-w-none max-h-full md:min-w-full mx-auto" src="./images/joseph-gara-hero-01.jpg" alt="Joseph Gara masthead" />
       </div>
       <footer className="flex flex-row justify-center items-center gap-3 p-6 pb-12">
