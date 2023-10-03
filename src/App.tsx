384d9a2d0b66733541583373eb14a41ce69741fb
Ryan Macalandag <rmacalandag@gmail.com>
2023-10-03 23:29:14 +1100

    fix height calc

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ function App() {
 
   return (
     <div className="flex flex-col w-full">
-      <header className="pt-12 pb-8 md:py-14 text-center text-slate-900">
-        <h3 className="py md:py-2 text-2xl md:text-3xl font-bold uppercase tracking-widest">This is</h3>
+      <header className="pt-10 pb-6 md:py-14 text-center text-slate-900">
+        <h3 className="py-0 md:py-2 text-2xl md:text-3xl font-bold uppercase tracking-widest">This is</h3>
         <h1 className="text-4xl md:text-6xl font-bold">Joseph Gara</h1>
       </header>
-      <div className="flex flex-row justify-center items-center h-3/5 overflow-hidden">
+      <div className="flex flex-row justify-center items-center h-[calc(60vh)] md:h-[calc(60vh)] overflow-hidden">
         <img className="object-cover max-w-none max-h-full md:min-w-full mx-auto" src="./images/joseph-gara-hero-01.jpg" alt="Joseph Gara masthead" />
       </div>
       <footer className="flex flex-row justify-center items-center gap-3 p-6 pb-12">
